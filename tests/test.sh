0f915b4452a78297b073af9e1ac63ca192e91f48
Hui Guo <higuo@ucdavis.edu>
2020-08-20 12:13:56 -0700

    added unit tests for the data sync feature in region transforamtion -- in debugging

--- tests/test.sh
+++ tests/test.sh
@@ -35,8 +35,21 @@ whole_transformation(){
   fi
   pLiner $test.c -r func-list.json --whole -o ${test}_wholetrans.c &>/dev/null -- $compflags
   if [ $? -ne 0 ]; then
-    echo -e "\e[31mError: $test.c -- print function failed\e[0m"
+    echo -e "\e[31mError: $test.c -- transformation failed\e[0m"
     pLiner $test.c -r func-list.json --whole -o ${test}_wholetrans.c -- $compflags
+    clean
+    return 2
+  fi
+  clean
+  return 0
+}
+
+region_transformation(){
+  test=$1
+  pLiner $test.c -r $test.json -o ${test}_trans.c &>/dev/null -- $compflags
+  if [ $? -ne 0 ]; then
+    echo -e "\e[31mError: $test.c -- transformation failed\e[0m"
+    pLiner $test.c -r $test.json -o ${test}_trans.c -- $compflags
     clean
     return 2
   fi
@@ -83,10 +96,11 @@ compile_and_compare(){
 }
 
 
-### test function transformation
 tfail=0
 tpass=0
 tnum=0
+
+### test function transformation
 echo -e "\ntesting pLiner - function transformation"
 for dir in $dirs; do
   echo "  $dir"
@@ -115,4 +129,35 @@ for dir in $dirs; do
   rm *_wholetrans.c
   cd ..
 done
+
+### test region transformation
+echo -e "\ntesting pLiner - function transformation"
+for dir in data-sync; do
+  echo "  $dir"
+  cd $dir
+  tests=`ls test_[0-9].c`
+  for test in $tests; do
+    tnum=$((tnum+1))
+    test=`echo $test | cut -d '.' -f 1`
+    echo "    $test"
+    region_transformation $test
+    if [ $? -eq 0 ]; then
+      echo -e "      - \e[32mtransformation completed\e[0m"
+    else 
+      echo -e "      - \e[31mtransformation failed\e[0m"
+    fi
+
+    compile_and_compare $test.c ${test}_trans.c  
+    if [ $? -eq 0 ]; then
+      echo -e "      - \e[32mcompile&compare completed\e[0m"
+      tpass=$((tpass+1))
+    else 
+      echo -e "      - \e[31mcompile&compare failed\e[0m"
+      tfail=$((tfail+1))
+    fi
+  done
+  #rm *_trans.c
+  cd ..
+done
+
 echo -e "\n  $tnum tests-- $tpass pass; $tfail failed"
