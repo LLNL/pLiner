2b1fc03cecc6de964f4617cf408015d89cc48453
aadarsh <aadarsh3996@gmail.com>
2021-03-18 21:31:51 -0700

    Varity compare script lists programs which failed to isolate inconsitency by pLiner

--- benchmarks/Varity/Varity-intel/compare_varity_results.py
+++ benchmarks/Varity/Varity-intel/compare_varity_results.py
@@ -4,6 +4,7 @@ import ast
 
 programs_match = []
 programs_mismatch = []
+pliner_isolation_failed_list = []
 class ParsedLine:
     def __init__(self, block_type, function_dict, output, transformed_output):
         self.block_type = block_type
@@ -26,15 +27,23 @@ for value in range(1, 51):
     try:
         print("\n\n")
         print("Program " +str(value)+" result:")
-        with open(file_name) as actual_file, open("reference/"+file_name) as reference_file:
+        with open(file_name) as actual_file, open("./reference/"+file_name) as reference_file:
             actual_file_success_line = ""
+            actual_file_flag = 0
             for line in actual_file:
+                if actual_file_flag ==0:
+                    actual_file_success_line = line
+                    actual_file_flag = 1
                 if "success" in line:
                     actual_file_success_line = line
 
 
+            reference_file_flag = 0
             reference_file_success_line = ""
             for line in reference_file:
+                if reference_file_flag==0:
+                    reference_file_success_line = line
+                    reference_file_flag = 1
                 if "success" in line:
                     reference_file_success_line = line
 
@@ -44,6 +53,12 @@ for value in range(1, 51):
                 continue
             actual_parsed_line = parse_line(actual_file_success_line)
             reference_parsed_line = parse_line(reference_file_success_line)
+
+            if "success" not in actual_file_success_line:
+                print("\tpLiner was unable to remove inconsistency for the file")
+                pliner_isolation_failed_list.append(value)
+
+
 
             if (actual_parsed_line.output ==  actual_parsed_line.transformed_output) and (actual_parsed_line.output == reference_parsed_line.output) and (actual_parsed_line.transformed_output and reference_parsed_line.transformed_output):
                 print("\tResults match")
@@ -63,6 +78,8 @@ for value in range(1, 51):
 print("\nResults summary")
 print("Total Number of programs matched: "+str(len(programs_match)))
 print("Total Number of programs which did not match: "+str(len(programs_mismatch)))
+
 if programs_mismatch:
     print("List of programs which did not match\n{}".format(str(programs_mismatch)))
-
+print("Total Number of programs for which pLiner was unable to remove inconsistency: "+str(len(pliner_isolation_failed_list)))
+print("List of programs for which pLiner was unable to remove inconsistency\n{}".format(str(pliner_isolation_failed_list)))
